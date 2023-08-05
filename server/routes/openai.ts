fa1d3d3823cc24234f6f80f3512d3393ee2db30c
John Gray <john.edwin.gray@gmail.com>
2023-08-06 11:42:10 +1200

    Refactor openai.ts to use a conversation array to store and retrieve chat messages.

--- server/routes/openai.ts
+++ server/routes/openai.ts
@@ -11,32 +11,38 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration)
 
+const conversation = [
+  { role: 'system', content: 'You are a helpful assistant.' }
+];
+
 async function getCompletion(prompt: string) {
+    conversation.push({ role: 'user', content: prompt });
+
     const chatCompletion = await openai.createChatCompletion({
       model: 'gpt-3.5-turbo',
-      messages: 
-      [
-          { role: 'system', content: 'You are a helpful assistant.' },
-          { role: 'user', content:    prompt }
-        ],
-    })
-    const data = chatCompletion.data.choices[0].message
-    return data
+      messages: conversation,
+    });
+
+    const data = chatCompletion.data.choices[0].message;
+    conversation.push({ role: 'assistant', content: data.content });
+
+    return data;
 }
 
 router.post('/', async (req, res) => {
-  const prompt = req.body.question
+  const prompt = req.body.question;
   
   try {
-    const data = await getCompletion(prompt)
-    res.status(200).json({ data })
+    const data = await getCompletion(prompt);
+    res.status(200).json({ data });
   } catch (error: unknown) {
     if (error instanceof Error) { 
-      console.log(error.message)
+      console.log(error.message);
     } else {    
-      res.status(500).json({ message: 'Something went wrong' })
+      res.status(500).json({ message: 'Something went wrong' });
     }
   }
-})
+});
+
 
 export default router
