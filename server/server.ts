97fff5269b63ecd781a5ffda9d806382b3d148fc
John Gray <john.edwin.gray@gmail.com>
2023-08-14 16:23:57 +1200

    rename routes file, remove console.log

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,7 @@
 import path from 'node:path';
 import express from 'express'
 import cors, { CorsOptions } from 'cors'
-import openAi from './routes/openai'
+import openAiRoutes from './routes/openAiRoutes'
 import bodyParser from 'body-parser';
 
 const server = express()
@@ -10,11 +10,8 @@ server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
 server.use(bodyParser.json())
 server.use(cors('*' as CorsOptions))
-// server.use((req, res, next) => {
-//   console.log('Request!!!!', req.url);
-//   next();
-// });
-server.use('/api/v1/openai', openAi)
+
+server.use('/api/v1/openai', openAiRoutes)
 
 if (process.env.NODE_ENV === 'production') {
   server.use('/assets', express.static(path.resolve(__dirname, '../assets')))
