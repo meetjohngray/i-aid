89eaa73039262b164bba61fd0831f59e9dd2ec54
John Gray <john.edwin.gray@gmail.com>
2023-08-19 10:51:27 +1200

    add basic css to chat component

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -14,18 +14,24 @@ const App = () => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>, message: string) => {
     e.preventDefault()
+    
     if(!message) return
     setIsTyping(true)
+    scrollTo(0, 1e10)
+    
     const msgs = chats
     msgs.push({role: "user", content: message})
     setChats(msgs)
     setMessage('')
+    
     getAnswer(chats)
       .then((res) => {
         const newMessage: Chat = res.body.output
+        console.log(newMessage)
         msgs.push(newMessage)
         setChats(msgs)
         setIsTyping(false)
+        scrollTo(0, 1e10)
       })
       .catch(e => {
         console.log(e)
@@ -33,44 +39,46 @@ const App = () => {
     }
 
   return (
+    <main>
+    <h1>I Aid</h1>
+
+    <section>
+      {chats && chats.length
+        ? chats.map((chat, index) => (
+            <p key={index} className={chat.role === "user" ? "user_msg" : ""}>
+              <span>
+                <b>{chat.role.toUpperCase()}</b>
+              </span>
+              <span>:</span>
+              <span>{chat.content}</span>
+            </p>
+          ))
+        : ""}
+    </section>
+
     <div className={isTyping ? "" : "hide"}>
-      <h1>Ask Me Anything</h1>
-        <section>
-        {chats && chats.length
-          ? chats.map((chat, index) => (
-              <p key={index} className={chat.role === "user" ? "user_msg" : ""}>
-                <span>
-                  <b>{chat.role.toUpperCase()}</b>
-                </span>
-                <span>:</span>
-                <span>{chat.content}</span>
-              </p>
-            ))
-          : ""}
-      </section>
-        <p>
-          <i>{isTyping ? "Typing" : ""}</i>
-        </p>
-        <form onSubmit={(e) => handleSubmit(e, message)} aria-label='Ask a question'>
-          {/* <label htmlFor='message'>Your Message</label> */}
-          <input
-            type='text' 
-            id='question' 
-            name='message'
-            value={message}
-            placeholder='Type your message here...'
-            onChange={e => setMessage(e.target.value)}
-          />
-        </form>
-      </div>
-    // <>
+      <p>
+        <i>{isTyping ? "Typing" : ""}</i>
+      </p>
+    </div>
+
+    <form action="" onSubmit={(e) => handleSubmit(e, message)}>
+      <input
+        type="text"
+        name="message"
+        value={message}
+        placeholder="Type a message here and hit Enter..."
+        onChange={(e) => setMessage(e.target.value)}
+      />
+    </form>
+  </main>
+)
     //   
     //   {isError && (
     //     <p style={{ color: 'red' }}>
     //       There was an error retrieving the greeting.
     //     </p>
     //   )}
-  )
 }
 
 export default App
